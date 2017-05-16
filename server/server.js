ce7c5423a0ec11fcdf491b98162f6c2a0baff676
Pranoy Biswas <pranoybiswas1994@yahoo.com>
2017-05-16 18:10:38 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(3000, ()=>{
+   console.log("Server is running on port 3000"); 
+});
